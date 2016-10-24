768bb49ee0b4aff38e9c004922802faa04661c70
Sam Larsen <samlarsen1@yahoo.co.uk>
2016-10-24 08:30:41 +0100

    First commit

---
+++ src/main/java/osm/org/DataswipeApplication.java
@@ -0,0 +1,12 @@
+package osm.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataswipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataswipeApplication.class, args);
+	}
+}
---
+++ src/test/java/osm/org/DataswipeApplicationTests.java
@@ -0,0 +1,16 @@
+package osm.org;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataswipeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
